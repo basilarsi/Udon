7b41c6006c070d12f5c0f3a0cc0c70ee3eb0bd35
Basil <basilarsi@gmail.com>
2026-08-06 16:02:30 -0600

    Blocked out the window class a bit

--- Udon/include/Udon/Core/Window.hpp
+++ Udon/include/Udon/Core/Window.hpp
@@ -5,10 +5,39 @@
 #ifndef UDON_WINDOW_HPP
 #define UDON_WINDOW_HPP
 
-#include "Udon/Pch.hpp"
+#include <optional>
+
+#include "Udon/SDL.hpp"
 
 namespace udon {
+  struct WindowCreateInfo final {
+  };
 
+  class UDON_API Window final {
+  public:
+    /**
+     * @brief Reads and validates a given WindowCreateInfo struct, then stores it for later initialization.
+     * @return True if the structure passed validation, false otherwise.
+     * @remarks If you send a struct, and it fails to validate, the window will refuse to construct until a valid struct is provided.
+     */
+    [[nodiscard]] static bool SendConfig(const WindowCreateInfo& createInfo);
+
+    /**
+     * @brief Attempts to create a window. You shouldn't call this unless you're using a custom app loop.
+     * @return True on success, false otherwise.
+     */
+    [[nodiscard]] static bool Construct();
+
+    /**
+     * @brief Destroys the window, you shouldn't call this unless you're using a custom app loop.
+     */
+    static void Teardown();
+
+  private:
+    static inline std::optional<WindowCreateInfo> _createInfo             = std::nullopt;
+    static inline bool                            _failedConfigValidation = false;
+    static inline SDL::Window                     _window;
+  };
 }
 
 #endif //UDON_WINDOW_HPP
